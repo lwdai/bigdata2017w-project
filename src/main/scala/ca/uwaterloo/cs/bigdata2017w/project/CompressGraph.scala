e6a2746dad1bb6dd7db0c3bd1fed58ce87335b15
Liwen Dai <lwdai1990@gmail.com>
2017-04-05 12:59:45 -0400

    aggregateMessages when collecting neighbours now uses UnrolledBuffer for O(1) concatenation

--- src/main/scala/ca/uwaterloo/cs/bigdata2017w/project/CompressGraph.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/project/CompressGraph.scala
@@ -28,6 +28,7 @@ import org.apache.spark.SparkConf
 import org.rogach.scallop._
 import org.apache.spark.graphx._
 
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import org.apache.spark.rdd.RDD
 
@@ -88,13 +89,14 @@ object CompressGraph {
      * Now every line is a unique vertexId followd by a sorted list of its out-neighbours.
      * This reduces the text file size by about a half.
      */
-    val graphStripes = graph.aggregateMessages[List[Long]](
+    val graphStripes: RDD[(Long, Array[Long])] = graph.aggregateMessages[mutable.UnrolledBuffer[Long]](
       triplet => {
-        triplet.sendToSrc(List(triplet.dstId))
+        triplet.sendToSrc(mutable.UnrolledBuffer(triplet.dstId))
       },
 
-      (msg1, msg2) => (msg1 ++ msg2)
-    )
+      // O(1) concat operation
+      (msg1, msg2) => (msg1.concat(msg2))
+    ).map( v => (v._1, v._2.toArray.sorted) )
 
     // Now compress the each list of out neighbours
     graphStripes.mapPartitions( iter => {
@@ -168,11 +170,11 @@ object CompressGraph {
       decompressedGraph.edges.sortBy( e => (e.srcId, e.dstId), true, 1 ).map( e => e.srcId + " " + e.dstId )
         .saveAsTextFile(args.output() + "-recover")
 
-      //if ( graphEqual(graph, decompressedGraph ) ) {
-      //  println("Compression/decompression test passed.")
-      //} else {
-      //  println("Compression/decompression test failed.")
-      //}
+      if ( graphEqual(graph, decompressedGraph ) ) {
+        println("Compression/decompression test passed.")
+      } else {
+        println("Compression/decompression test failed.")
+      }
       println("Please check the decompressed graph output at " + args.output() + "-recover" )
     }
 
