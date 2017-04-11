c54580dfcfc55d061224e2fc74a2765531dfe4eb
Liwen Dai <lwdai1990@gmail.com>
2017-04-11 02:37:24 -0400

    single root betweenness

--- src/main/scala/ca/uwaterloo/cs/bigdata2017w/project/KBetweenness.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/project/KBetweenness.scala
@@ -38,37 +38,60 @@ class KBetweennessConfig(args: Seq[String]) extends ScallopConf(args) with Token
 object KBetweenness {
   val log = Logger.getLogger(getClass().getName())
 
-  def initBFS( graph: Graph[(Int, Long, Double), (Double, Double)], root: Long):
-      Graph[(Int, Long, Double), (Double, Double)] = {
+  // VD = (depth, #SP, credit); ED = credit_acc
+  def initBFS( graph: Graph[(Int, Long, Double, Double), Double], root: Long):
+      Graph[(Int, Long, Double, Double), Double] = {
 
-    graph.mapVertices[(Int, Long, Double)]((vid, v) =>
+    graph.mapVertices[(Int, Long, Double, Double)]((vid, v) =>
       ( if (vid == root) 0 else Integer.MAX_VALUE,
         if (vid == root) 1 else 0,
-        1.0) ).mapEdges( e => (e.attr._1, 0.0)  )
+        0.0,
+        0.0) )
   }
 
+  // message = (depth of parent, number of shortest paths to root, isLeaf)
   // If receives a message s.t. isLeaf = false, set credit to 0.0
-  def bfsVprog(vid: Long, vd: (Int, Long, Double), msg: (Int, Long, Boolean)): (Int, Long, Double) = {
+  def bfsVprog(vid: Long, vd: (Int, Long, Double, Double), msg: (Int, Long)): (Int, Long, Double, Double) = {
     ( if (vd._1 == Integer.MAX_VALUE && msg._1 < Integer.MAX_VALUE) msg._1 + 1 else vd._1,
       vd._2 + msg._2,
-      if (msg._3) vd._3 else 0.0 )
+      0.0,
+      0.0 )
   }
 
-  def bfsSendMsg(e: EdgeTriplet[(Int, Long, Double), (Double, Double)]): Iterator[(VertexId, (Int, Long, Boolean))] = {
+  def bfsSendMsg(e: EdgeTriplet[(Int, Long, Double, Double), Double]): Iterator[(VertexId, (Int, Long))] = {
     if ( e.srcAttr._1 < Integer.MAX_VALUE && e.dstAttr._1 == Integer.MAX_VALUE ) {
       // only send message if source vertex is visited and dst vertex is unvisited
-      Iterator( (e.dstId, (e.srcAttr._1, e.srcAttr._2, true) ), ( e.srcId, (0, 0L, false) )  )
+      Iterator( (e.dstId, (e.srcAttr._1, e.srcAttr._2) ), ( e.srcId, (0, 0L) )  )
     } else {
       Iterator.empty
     }
   }
 
-  def bfsMergeMsg(msg1: (Int, Long, Boolean), msg2: (Int, Long, Boolean)): (Int, Long, Boolean) = {
-    ( if (msg1._1 < msg2._1) msg1._1 else msg2._1,
+  def bfsMergeMsg(msg1: (Int, Long), msg2: (Int, Long)): (Int, Long) = {
+    ( // depth of parent
+      if (msg1._1 < msg2._1) msg1._1 else msg2._1,
+      // # SP
       if (msg1._1 < msg2._1) msg1._2
       else if (msg1._1 > msg2._1) msg2._2
-      else msg1._2 + msg2._2,
-      msg1._3 && msg2._3 )
+      else msg1._2 + msg2._2)
+  }
+
+  // Add credit to the vertex
+  def bottomUpVprog(vid: Long, vd: (Int, Long, Double, Double), msg: Double): (Int, Long, Double, Double) = {
+    ( vd._1, vd._2, vd._3 + vd._4, msg )
+  }
+
+  def bottomUpSendMsg(e: EdgeTriplet[(Int, Long, Double, Double), Double]): Iterator[(VertexId, Double)] = {
+    if ( e.srcAttr._1 == e.dstAttr._1 - 1 && e.dstAttr._4 > 0.0 ) {
+      // src is parent of dst
+      Iterator( (e.srcId, e.dstAttr._4 * e.srcAttr._2.toDouble / e.dstAttr._2.toDouble) )
+    } else {
+      Iterator.empty
+    }
+  }
+
+  def bottomUpMergeMsg( msg1: Double, msg2: Double ): Double = {
+    msg1 + msg2
   }
 
   def main(argv: Array[String]): Unit = {
@@ -82,33 +105,45 @@ object KBetweenness {
 
     //val graph: Graph[Int, Int] = GraphLoader.edgeListFile(sc, args.input()).cache()
 
-    // VD = (depth, number of shortest paths to root, credit); ED = (credit_acc, credit)
+    // VD = (depth, number of shortest paths to root, credit); ED = credit_acc
     //var graph: Graph[(Int, Long, Double), (Double, Double)] = CompressGraph.loadCompressedGraph(args.input(),
     //  sc)
-    var graph: Graph[(Int, Long, Double), (Double, Double)] = GraphLoader.edgeListFile(sc, args.input())
-      .mapVertices[(Int, Long, Double)]( (vid, v) => (0, 0, 0.0) ).
-      mapEdges( e => (0.0, 0.0)).cache()
+    var graph: Graph[(Int, Long, Double, Double), Double] = GraphLoader.edgeListFile(sc, args.input())
+      .mapVertices[(Int, Long, Double, Double)]( (vid, v) => (0, 0, 0.0, 0.0) ).
+      mapEdges( e => 0.0).cache()
 
     val outputDir = new Path(args.output())
     val deleted = FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
     if (deleted) println("output directory is deleted.")
 
-
-
     1 to args.seeds() foreach { _ => {
       //val root: Long = graph.pickRandomVertex()
       //println("Picked vertexID " + root + " as root for BFS.")
       val root = 1
-      // message = (depth of parent, number of shortest paths to root, isLeaf)
-      graph = initBFS(graph, root).pregel[(Int, Long, Boolean)]( (Integer.MAX_VALUE, 0, true), args.diameter(),
-        EdgeDirection.Out)(
-        bfsVprog, bfsSendMsg, bfsMergeMsg)
+      // message = (depth of parent, number of shortest paths to root)
+      graph = initBFS(graph, root).
+        // BFS
+        pregel[(Int, Long)]( (Integer.MAX_VALUE, 0), args.diameter(), EdgeDirection.Out)(
+        bfsVprog, bfsSendMsg, bfsMergeMsg).
+
+        pregel[Double]( 1.0, args.diameter(), EdgeDirection.In)(bottomUpVprog, bottomUpSendMsg, bottomUpMergeMsg)
+        .mapVertices( (vid, vd) => (vd._1, vd._2, vd._3 + vd._4, 0.0) )
     } }
 
     // "vid depth #SP credit", where leaves have credit of 1
-    graph.mapVertices( (vid, vd) => vid + " " + vd._1 + " " + vd._2 + " " + vd._3 ).vertices.saveAsTextFile(args
-      .output())
+    //graph.mapVertices( (vid, vd) =>  vd._1 + " " + vd._2 + " " + vd._3 ).vertices.saveAsTextFile(args
+    //  .output())
+/*
+    graph.triplets.map( et => {
+      val edgeCredit: Double =
+        if (et.srcAttr._1 == et.dstAttr._1 - 1) et.dstAttr._3 * et.srcAttr._2.toDouble / et.dstAttr._2.toDouble
+        else 0.0
+
+      et.srcId + " " + et.dstId + " " +
+        (edgeCredit + et.attr) } ).saveAsTextFile(args.output())
+*/
+    graph.vertices.map( v => v._1 + " " + v._2._1 + " " + v._2._2 + " " + v._2._3 ).saveAsTextFile(args.output())
 
     sc.stop()
   }
